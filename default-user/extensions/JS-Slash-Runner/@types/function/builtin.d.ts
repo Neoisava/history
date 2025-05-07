48d72594c5e6029998e3e58ecbefd0ab0cbb4a86
git-batch <git-batch@github.com>
2025-05-07 12:38:28 +0000

    update

--- default-user/extensions/JS-Slash-Runner/@types/function/builtin.d.ts
+++
@@ -1,7 +0,0 @@
-declare global {
-  const builtin: {
-    saveSettings: () => Promise<void>;
-    reloadEditor: (file: string, load_if_not_selected?: boolean) => void;
-    reloadEditorDebounced: (file: string, load_if_not_selected?: boolean) => void;
-  };
-}
--- default-user/extensions/JS-Slash-Runner/src/function/builtin.ts
+++
@@ -1,8 +0,0 @@
-import { reloadEditor, reloadEditorDebounced } from '@/compatibility';
-import { saveSettings } from '@sillytavern/script';
-
-export const builtin = {
-  saveSettings,
-  reloadEditor,
-  reloadEditorDebounced,
-};
